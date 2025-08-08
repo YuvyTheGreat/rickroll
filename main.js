42c15aa8f80a825dc1e79b08889ab8dff678408d
YuvyTheGreat <yuvy113344@gmail.com>
2025-08-08 11:42:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+        const audio = document.getElementById('rickroll');
+        const videoContainer = document.querySelector('.videodiv');
+        
+        videoContainer.addEventListener('animationend', ()=> {
+                audio.play().catch(err=> {
+                        console.log("The stupid greedy person's audio didn't play, they might need to interact with the video XD.");
+                    });
+            });
+    });
+
+  document.addEventListener('DOMContentLoaded', function() {
+          const counter = new Counter({ workspace: 'rickroll-counting' });
+          
+          counter.up('rickroll')
+                  .then(result => {
+                          document.getElementById('rickrollcount').innerHTML = `Rickrolls: ${result.value} <br> <a href="HelloHathi.com">hellohathi.com</a>`;
+                  })
+                  .catch(error => {
+                          console.error('Error tracking page view:', error);
+                          document.getElementById('rickrollcount').textContent = 'Error loading count';
+                  });
+  });
